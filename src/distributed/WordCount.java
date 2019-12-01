9e4394705420ea84ac0f34606cc1652dc88b468b
Sabeeh Shah <sabeeh.shah@hotmail.com>
2019-12-01 15:32:24 -0500

    Update WordCount.java
    Split by comma for csv input file

--- src/distributed/WordCount.java
+++ src/distributed/WordCount.java
@@ -34,7 +34,7 @@ public class WordCount {
 	public static class MapForWordCount extends Mapper<LongWritable, Text, Text, IntWritable> {
 		public void map(LongWritable key, Text value, Context con) throws IOException, InterruptedException {
 			String line = value.toString();
-			String[] words = line.split(" ");
+			String[] words = line.split(",");
 			for (String word : words) {
 				Text outputKey = new Text(word.toUpperCase().trim());
 				IntWritable outputValue = new IntWritable(1);
